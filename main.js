bbba7513d6caf38130b6519ef6047dd6c338eb40
john-lay <john_lay@hotmail.co.uk>
2018-10-04 21:06:07 +0100

    Calculate down-right direction
    Add key up event.
    Add constants for keyCodes.

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
-document.onkeydown = function (e) {
+var rightPressed = false;
+var downPressed = false;
+
+var LEFT = 37;
+var UP = 38;
+var RIGHT = 39;
+var DOWN = 40;
+
+document.onkeydown = function(e) {
     e = e || window.event;
-    console.log(e.keyCode);
+    console.log("key down = ", e.keyCode);
 
     switch(e.keyCode) {
-        case 37: 
-            showInput('l');
+        case RIGHT: 
+            rightPressed = true;
         break;
-        case 39: 
-            showInput('r');
+        case DOWN: 
+            downPressed = true;
         break;
-        case 38: 
-            showInput('u');
-        break;
-        case 40: 
-            showInput('d');
-        break;
+    }
+}
+
+document.onkeyup = function(e) {
+    e = e || window.event;
+    console.log("key up = ", e.keyCode);
+
+    if(e.keyCode === DOWN) {
+        downPressed = false;
+        showInput('d');
+
+        if(rightPressed) showInput('dr');        
+    }
+
+    if(e.keyCode === RIGHT) {
+        rightPressed = false;
+        showInput('r');
+
+        if(downPressed) showInput('dr');
     }
 }
 
