b65ea9076f505d0144945d5b77996e0c7245d15c
john-lay <john_lay@hotmail.co.uk>
2018-10-09 20:42:37 +0100

    Add input buffer
    Add logic to check buffer for known special move combination.

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 var rightPressed = false;
 var downPressed = false;
 
-var LEFT = 37;
-var UP = 38;
-var RIGHT = 39;
-var DOWN = 40;
+// var LEFT_KEY = 37;
+// var UP_KEY = 38;
+var RIGHT_KEY = 39;
+var DOWN_KEY = 40;
+
+var inputBuffer = [];
+
+// using japanese street fighter notation for direction
+// https://sonichurricane.com/articles/sfnotation.html
+var direction = {
+    down: { name: 'd', notation: 2 },
+    downRight: { name: 'dr', notation: 3 },
+    right: { name: 'r', notation: 6 }
+}
 
 document.onkeydown = function(e) {
     e = e || window.event;
     console.log("key down = ", e.keyCode);
 
     switch(e.keyCode) {
-        case RIGHT: 
+        case RIGHT_KEY: 
             rightPressed = true;
         break;
-        case DOWN: 
+        case DOWN_KEY: 
             downPressed = true;
         break;
     }
@@ -24,24 +34,48 @@ document.onkeyup = function(e) {
     e = e || window.event;
     console.log("key up = ", e.keyCode);
 
-    if(e.keyCode === DOWN) {
+    if(e.keyCode === DOWN_KEY) {
         downPressed = false;
-        showInput('d');
+        addInput(direction.down);
 
-        if(rightPressed) showInput('dr');        
+        if(rightPressed) addInput(direction.downRight);        
     }
 
-    if(e.keyCode === RIGHT) {
+    if(e.keyCode === RIGHT_KEY) {
         rightPressed = false;
-        showInput('r');
+        addInput(direction.right);
 
-        if(downPressed) showInput('dr');
+        if(downPressed) addInput(direction.downRight);
     }
 }
 
-function showInput(direction) {
+function addInput(direction) {
+    // update buffer
+    inputBuffer.push(direction.notation);
+    
+    // check for special moves
+    checkBuffer();
+    
+    // update UI
+    drawInput(direction.name);
+}
+
+function drawInput(directionName) {
     var node = document.createElement("div");
-    node.className = "icon " + direction;
+    node.className = "icon " + directionName;
 
     document.getElementById("Container").appendChild(node);
+}
+
+function checkBuffer() {
+    for(var i=0; i<inputBuffer.length; i++) {
+        // check for 3 direction special moves
+        if(i+2 <= inputBuffer.length) {
+            if(inputBuffer[i] === direction.down.notation &&
+               inputBuffer[i+1] === direction.downRight.notation &&
+               inputBuffer[i+2] === direction.right.notation) {
+                    console.log('Hadoken!');
+                }
+        }
+    }
 }
